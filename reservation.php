c3edc0019644052a91fdb11daf824cc827782562
fatemeasghari1992 <41268917+fatemeasghari1992@users.noreply.github.com>
2018-07-16 15:55:39 +0430

    Add files via upload

--- reservation.php
+++ reservation.php
@@ -1,18 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title></title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/style.css">
-    <script src="js/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-</head>
-<body>
-<div class='container'>
-    <div class='row'>
-        <div class='col-md-8 col-md-offset-3'>
 <?
 require_once('main.php');
 
@@ -22,17 +7,23 @@ $studentNumber = $_COOKIE["studentNumber"];
 if(authenticate($studentNumber,$session)){
     $db = Db::getInstance();
     $record = $db->first("SELECT DayOfWeek,Name FROM Foods WHERE ID='$foodId'");
-    $isReserved = $db->first("SELECT id FROM food_reservation WHERE foodId='$foodId'");
+    $DayOfWeek = $record['DayOfWeek'];
+    echo "<div style='text-align: center'>";
     if ($record == null){
         $message = "غذایی با این شماره وجود ندارد";
         require_once("failed.php");
-
-        echo "<a href='index.php' class='btn btn-warning'>بازگشت به صفحه انتخاب غذا</a>";
-        //exit;
-    } else if(isset($isReserved)) {
-        $message = "این وعده قبلا رزرو شده است";
-        require_once("succeed.php");
-        echo "<a href='index.php' class='btn btn-warning'>بازگشت به صفحه انتخاب غذا</a>";
+        br();
+        br();
+        br();
+        echo "<a href='index.php'>بازگشت به صفحه انتخاب غذا</a>";
+        exit;
+    } else if(!Day($DayOfWeek)) {
+        $message = "شما در بازه‌ی زمانی نیستید که بتوانید این غذا را رزرو کنید";
+        require_once("failed.php");
+        br();
+        br();
+        br();
+        echo "<a href='index.php'>بازگشت به صفحه انتخاب غذا</a>";
         exit;
     }else{
         $DayOfWeek = $record['DayOfWeek'];
@@ -40,23 +31,14 @@ if(authenticate($studentNumber,$session)){
         $reserved = $db->insert("INSERT INTO food_reservation (foodId,studentNumber,Name,DayOfWeek) values ('$foodId','$studentNumber','$Name','$DayOfWeek')");
         $message = "غذای شما با موفقیت ثبت شد";
         require_once("succeed.php");
-
-        echo "<a href='index.php' class='btn btn-warning'>بازگشت به صفحه انتخاب غذا</a>";
-      //  exit;
+        br();
+        br();
+        br();
+        echo "<a href='index.php'>بازگشت به صفحه انتخاب غذا</a>";
+        exit;
     }
-}else{
-    $message = "شماره دانشجویی شما ثبت نشده است.لطفا به صفحه ورود مراجعه نمایید";
-    require_once("failed.php");
-
-    echo "<a href='http://127.0.0.1:8083/index.php' class='btn btn-danger col-md-offset-2'>بازگشت به صفحه ورود</a>";
-
-    //exit;
+    echo "</div>";
 }
 
 
 ?>
-</div>
-</div>
-</div>
-</body>
-</html>
